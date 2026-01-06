afbe73791d6f66b30c831e82a8fdda40dbc02836
wilma <mornlitoralis@gmail.com>
2026-01-06 16:57:25 +0700

    some changes

--- query.sql
+++ query.sql
@@ -4,6 +4,22 @@ SHOW TABLES;
 desc buku15841;
 desc penerbit15841;
 
+CREATE VIEW view5_15841 AS SELECT n.nama_neg_pembuat
+FROM negara15841 n
+LEFT JOIN penerbit15841 p
+    ON p.negara_id = n.negara_id
+WHERE p.penerbit_id IS NULL;
+
+CREATE VIEW view6_15841 AS SELECT n.nama_neg_pembuat, p.nama AS nama_penerbit from negara15841 n
+LEFT JOIN penerbit15841 p ON p.negara_id = n.negara_id;
+
+/* LEFT JOIN: all data must be displayed. If there is no data on the right side, enter NULL  */
+SELECT n.nama_neg_pembuat
+FROM negara15841 n
+lEFT JOIN penerbit15841 p
+    ON p.negara_id = n.negara_id
+WHERE p.penerbit_id IS NULL;
+
 SELECT * FROM view1_15841;
 
 SELECT * FROM view2_15841;
@@ -15,15 +31,3 @@ SELECT * FROM view4_15841;
 SELECT * FROM view5_15841;
 
 SELECT * FROM view6_15841;
-
-
-
-CREATE VIEW view5_15841 AS SELECT n.nama_neg_pembuat
-FROM negara15841 n
-LEFT JOIN penerbit15841 p
-    ON p.negara_id = n.negara_id
-WHERE p.penerbit_id IS NULL;
-
-CREATE VIEW view6_15841 AS SELECT n.nama_neg_pembuat, p.nama AS nama_penerbit from negara15841 n
-LEFT JOIN penerbit15841 p ON p.negara_id = n.negara_id;
-
