6ffd3c4357e5fb4b3930cf2798259914f0f1adca
wilma <mornlitoralis@gmail.com>
2026-01-06 16:45:49 +0700

    finishing the assignment

--- query.sql
+++ query.sql
@@ -1,28 +1,29 @@
-use koleksi_buku15841
-
+-- Active: 1765006836152@@localhost@3306@koleksi_buku15841
 SHOW TABLES;
 
 desc buku15841;
 desc penerbit15841;
 
 SELECT * FROM view1_15841;
+
 SELECT * FROM view2_15841;
+
 SELECT * FROM view3_15841;
 
-SELECT n.nama_neg_pembuat AS negara
-FROM negara15841 n
-JOIN penerbit15841 p
-    ON p.negara_id = n.negara_id
-LEFT JOIN buku_penerbit15841 bp
-    ON bp.penerbit_id = p.penerbit_id
-GROUP BY n.nama_neg_pembuat
-HAVING COUNT(bp.buku_id) = 0
-ORDER BY n.nama_neg_pembuat ASC;
+SELECT * FROM view4_15841;
 
-SELECT DISTINCT n.nama_neg_pembuat
+SELECT * FROM view5_15841;
+
+SELECT * FROM view6_15841;
+
+
+
+CREATE VIEW view5_15841 AS SELECT n.nama_neg_pembuat
 FROM negara15841 n
-JOIN penerbit15841 p 
+LEFT JOIN penerbit15841 p
     ON p.negara_id = n.negara_id
-LEFT JOIN buku_penerbit15841 bp 
-    ON bp.penerbit_id = p.penerbit_id
-WHERE bp.penerbit_id IS NULL;
+WHERE p.penerbit_id IS NULL;
+
+CREATE VIEW view6_15841 AS SELECT n.nama_neg_pembuat, p.nama AS nama_penerbit from negara15841 n
+LEFT JOIN penerbit15841 p ON p.negara_id = n.negara_id;
+
