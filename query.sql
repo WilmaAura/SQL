c74483d98f8d43c219a14b0f53b89339aacb7fd0
wilma <mornlitoralis@gmail.com>
2026-03-09 16:38:07 +0700

    add sbd1 and some changes

---
+++ query.sql
@@ -0,0 +1,37 @@
+-- Active: 1765006836152@@localhost@3306@koleksi_buku15841
+SHOW TABLES;
+
+desc buku15841;
+desc penerbit15841;
+
+CREATE VIEW view5_15841 AS SELECT n.nama_neg_pembuat
+FROM negara15841 n
+LEFT JOIN penerbit15841 p
+    ON p.negara_id = n.negara_id
+WHERE p.penerbit_id IS NULL;
+
+CREATE VIEW view6_15841 AS SELECT n.nama_neg_pembuat, p.nama AS nama_penerbit from negara15841 n
+LEFT JOIN penerbit15841 p ON p.negara_id = n.negara_id;
+
+/* LEFT JOIN: all data must be displayed. If there is no data on the right side, enter NULL  */
+SELECT n.nama_neg_pembuat
+FROM negara15841 n
+lEFT JOIN penerbit15841 p
+    ON p.negara_id = n.negara_id
+WHERE p.penerbit_id IS NULL;
+
+
+
+
+SELECT * FROM view1_15841;
+
+SELECT * FROM view2_15841;
+
+SELECT * FROM view3_15841;
+
+SELECT * FROM view4_15841;
+
+SELECT * FROM view5_15841;
+
+SELECT * FROM view6_15841;
+
---
+++ sbd1.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE db_usaha;
+
+show databases;
+
+CREATE TABLE branch (
+    id_cabang CHAR(4) PRIMARY KEY,
+    alamat VARCHAR(50) NOT NULL,
+    kota VARCHAR(50) NOT NULL,
+    alamat VARCHAR(50) NOT NULL,
+    
+)
