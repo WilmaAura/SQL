37c6b29febdc7dc8a3c75cec1e060d4fb9824aaf
wilma <mornlitoralis@gmail.com>
2026-04-07 11:03:15 +0700

    add some file and changes

---
+++ sbd/sbd2.sql
@@ -0,0 +1,83 @@
+-- Active: 1765006836152@@localhost@3306@office
+show databases;
+
+show tables;
+
+create table Branch(
+    branchNo char(4) primary key,
+    street varchar(50),
+    city varchar(20),
+    postcode varchar(10)
+);
+
+create table Staff(
+    staffNo varchar(10) primary key,
+    fName varchar(15), 
+    lname varchar(15),
+    position varchar(10),
+    sex char(1),
+    DOB varchar(10),
+    salary int,
+    branchNo char(4)
+);
+
+INSERT INTO Branch VALUES
+('B001', 'Jl Soekarno Hatta', 'Jakarta', '12345'),
+('B002', 'Jl Setiabudi', 'Bandung', '23451'),
+('B003', 'Jl Pahlawan', 'Semarang', '51234');
+
+INSERT INTO Staff VALUES
+('S001', 'Erick', 'Susanto', 'Manager', 'M', '1988-11-10', 8000000, 'B001'),
+('S002', 'Ayu', 'Susanti', 'Supervisor', 'F', '1991-12-05', 6000000, 'B002'),
+('S003', 'Dewangga', 'Saputro', 'Assistant', 'M', '1990-01-26', 3500000, 'B003');
+
+alter table Staff change position position varchar(10) NOT NULL;
+
+alter table Staff change sex sex CHAR(1) NOT NULL CHECK (sex IN('M', 'F'));
+
+desc Staff;
+
+drop table pegawai;
+
+-- SALAH
+-- CREATE TABLE pegawai (
+--     nip CHAR(5) PRIMARY KEY,
+--     Nama VARCHAR(25) NOT NULL,
+--     Jns_kel CHAR(1) NOT NULL CHECK(jns_kel IN ('L', 'P')),
+--     Usia INT NOT NULL,
+--     Gaji INT,
+--     branchNo CHAR(4) NOT NULL, 
+--     Id_position INT DEFAULT 1,
+
+--     CONSTRAINT fk_branch FOREIGN KEY(branchNo) REFERENCES Branch(branchNO),
+--     CONSTRAINT fk_posisi FOREIGN KEY(Id_position) REFERENCES Position(ID_position)  
+-- );
+
+CREATE TABLE pegawai (
+    nip CHAR(5) PRIMARY KEY,
+    Nama VARCHAR(25) NOT NULL,
+    Jns_kel CHAR(1) NOT NULL CHECK (Jns_kel IN ('L', 'P')),
+    Usia INT NOT NULL CHECK (Usia >= 18),
+    ID_position INT DEFAULT 1,
+    Gaji INT NULL,
+    branchNo CHAR(4) NOT NULL, 
+    CONSTRAINT fk_posisi FOREIGN KEY (ID_position) REFERENCES `Position`(ID_osition),
+    CONSTRAINT fk_cabang FOREIGN KEY (branchNo) REFERENCES Branch(branchNo)
+);
+
+desc `Position`;
+
+-- alter table pegawai
+-- ADD CONSTRAINT fk_branch
+-- FOREIGN KEY (branchNo)
+-- REFERENCES Branch(BranchNo);
+
+-- ALTER TABLE pegawai ADD COLUMN Id_position INT DEFAULT 1 AFTER Usia;
+-- ALTER TABLE pegawai 
+-- ADD CONSTRAINT fk_position 
+-- FOREIGN KEY (Id_position) REFERENCES Position(ID_position);
+
+-- show tables;
+-- desc Position;
+
+
---
+++ sbd/storedProcedure.sql
@@ -0,0 +1,52 @@
+-- Active: 1765006836152@@localhost@3306@latSBD_3
+create database latSBD_3;
+
+create table pelanggan(
+    ID char(3) PRIMARY KEY,
+    nama varchar(25) NOT NULL,
+    kota varchar(25) NOT NULL,
+    gaji int
+);
+
+INSERT INTO pelanggan (ID, nama, kota, gaji) VALUES
+('001', 'Nathan', 'Surabaya', 5000000),
+('002', 'Andini', 'Solo', 9000000),
+('003', 'Sita', 'Bandung', 8000000),
+('004', 'Soni', 'Bekasi', 5000000),
+('005', 'Dedi', 'Palembang', 6000000);
+
+SELECT * FROM pelanggan;
+
+DELIMITER // 
+CREATE PROCEDURE SP1()
+BEGIN
+SELECT * FROM pelanggan;
+END//
+DELIMITER;
+
+CALL SP1;
+
+DELIMITER//
+
+CREATE PROCEDURE SP2(ID CHAR(3), nama varchar(25), kota varchar(25), gaji int)  
+BEGIN
+INSERT INTO pelanggan VALUES (ID, nama, kota, gaji);
+SELECT * FROM pelanggan;
+END//
+DELIMITER;
+
+CALL SP2('008', 'Roni', 'Magelang', 10000000);
+
+DELIMITER //
+CREATE PROCEDURE SP3(gaji_lama int, gaji_baru int)
+BEGIN
+UPDATE pelanggan set gaji = gaji_baru where gaji = gaji_lama;
+SELECT nama, gaji FROM pelanggan;
+END//
+DELIMITER;
+
+CALL SP3(5000000, 5500000);
+
+DROP PROCEDURE SP3;
+
+SHOW PROCEDURE status;
