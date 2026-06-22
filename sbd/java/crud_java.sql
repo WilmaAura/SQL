8825adb23812b032b4190c37728ccf62cfdf8aa0
wilma <mornlitoralis@gmail.com>
2026-06-22 11:13:26 +0700

    add java folder for responsi

---
+++ sbd/java/crud_java.sql
@@ -0,0 +1,12 @@
+-- Active: 1765006836152@@localhost@3306@obat
+
+CREATE TABLE obat (
+    kode_obat CHAR(15) PRIMARY KEY,
+    nama_obat VARCHAR(50),
+    satuan_obat CHAR(10),
+    jml_stok int,
+    jml_min int,
+    expired_date date,
+    supplier CHAR (15)
+);
+gi
